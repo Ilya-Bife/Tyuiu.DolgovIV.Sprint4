792e9e5b12fe8d9a24550559532f558501dba64a
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-11-02 14:23:55 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.DolgovIV.Sprint4.Task0.V20.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Tyuiu.DolgovIV.Sprint4.Task0.V20.Lib;
+
+namespace Tyuiu.DolgovIV.Sprint4.Task0.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 4, 8, 7, 6, 5, 8, 2, 4, 3, 2 };
+
+            Assert.AreEqual(24576, ds.GetMultEvenArrEl(array));
+        }
+    }
+}
---
+++ Tyuiu.DolgovIV.Sprint4.Task0.V20/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.DolgovIV.Sprint4.Task0.V20.Lib;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x1 = Convert.ToInt32(Console.ReadLine());
+        int x2 = Convert.ToInt32(Console.ReadLine());
+        int x3 = Convert.ToInt32(Console.ReadLine());
+        int x4 = Convert.ToInt32(Console.ReadLine());
+        int x5 = Convert.ToInt32(Console.ReadLine());
+        int x6 = Convert.ToInt32(Console.ReadLine());
+        int x7 = Convert.ToInt32(Console.ReadLine());
+        int x8 = Convert.ToInt32(Console.ReadLine());
+        int x9 = Convert.ToInt32(Console.ReadLine());
+        int x10 = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int[] array = { 4, 8, 7, 6, 5, 8, 2, 4, 3, 2 };
+        Console.WriteLine(ds.GetMultEvenArrEl(array));
+
+
+        Console.ReadKey();
+    }
+}
