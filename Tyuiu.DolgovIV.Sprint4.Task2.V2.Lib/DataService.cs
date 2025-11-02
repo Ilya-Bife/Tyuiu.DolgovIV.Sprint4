d05f94e06aa38dc65b217935ce0a0b4a4e8cc9d3
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-11-02 15:36:06 +0500

    Task2

---
+++ Tyuiu.DolgovIV.Sprint4.Task2.V2.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.DolgovIV.Sprint4.Task2.V2.Lib
+{
+    public class DataService : ISprint4Task2V2
+    {
+        public int Calculate(int[] array)
+        {
+            int c = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    c *= array[i];
+                }
+            }
+            return c;
+        }
+    }
+}
---
+++ Tyuiu.DolgovIV.Sprint4.Task2.V2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.DolgovIV.Sprint4.Task2.V2.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Random random = new Random();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.Write("Введите кол-во элементов массива");
+        int len = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[len];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1,9);
+            Console.Write(array[i] + " ");
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine(ds.Calculate(array));
+
+
+        Console.ReadKey();
+    }
+}
