924ad70fb077defe02a34cf8ee8097db5ba494a7
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-11-02 16:35:45 +0500

    Task3

---
+++ Tyuiu.DolgovIV.Sprint4.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.DolgovIV.Sprint4.Task3.V3.Lib;
+
+namespace Tyuiu.DolgovIV.Sprint4.Task3.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[,] arr =
+            {
+                { 9, 4, 5, 4, 8},
+                { 7, 6, 7, 7, 4,},
+                { 4, 4, 3, 5, 6,},
+                { 6, 5, 9, 4, 9,},
+                { 9, 7, 8, 7, 7}
+            };
+
+            DataService ds = new DataService();
+            Assert.AreEqual(ds.Calculate(arr), 14);
+        }
+    }
+}
---
+++ Tyuiu.DolgovIV.Sprint4.Task3.V3/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.DolgovIV.Sprint4.Task3.V3.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] arr =
+        {
+            { 9, 4, 5, 4, 8},
+            { 7, 6, 7, 7, 4,},
+            { 4, 4, 3, 5, 6,},
+            { 6, 5, 9, 4, 9,},
+            { 9, 7, 8, 7, 7}
+         };
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine(ds.Calculate(arr));
+
+
+        Console.ReadKey();
+    }
+}
